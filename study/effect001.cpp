82e374806d48e7b13c759ac9b977043954709504
createjin <createsejin@gmail.com>
2024-04-27 18:57:38 +0900

    2024-04-27-002

--- study/effect001.cpp
+++ study/effect001.cpp
@@ -16,4 +16,18 @@ void study001() {
   Widget w3 = w2;
 }
 
+class GamePlayer {
+  // static const int NumTurns{5};
+  // enum hack
+  enum { NumTurns = 5 };
+  int scores[NumTurns];
+};
+
+const double CostEstimate::FudgeFactor = 1.35;
+
+template <typename T>
+inline void callWithMax(const T& a, const T& b) {
+  f(a > b ? a : b);
+}
+// next: item3 p.17
 }  // namespace study001
--- study/effect001.h
+++ study/effect001.h
@@ -2,4 +2,8 @@
 
 namespace study001 {
 void study001();
-}
+
+class CostEstimate {
+  static const double FudgeFactor;
+};
+}  // namespace study001
